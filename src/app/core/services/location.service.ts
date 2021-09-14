69841e826e4ee2eaab567974f22e89b3cfc39bf4
Ziyad Umar <ziyad96@gmail.com>
2021-09-14 20:43:52 +0200

    Add service: Location service

---
+++ src/app/core/services/location.service.ts
@@ -0,0 +1,34 @@
+
+import { Observer ,  Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LocationService {
+    latitude: number;
+    longitude: number;
+
+    constructor(private http: HttpClient) { }
+
+    getCurrentPosition(): Observable<any> {
+        return new Observable((observer: Observer<any>) => {
+            // Invokes getCurrentPosition method of Geolocation API.
+            navigator.geolocation.getCurrentPosition(
+                (position: any) => {
+                    observer.next(position);
+                    observer.complete();
+                },
+                (error: any) => {
+                    observer.error(error);
+                }
+            );
+        });
+    }
+
+    setLocation(latitude: number, longitude: number) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
